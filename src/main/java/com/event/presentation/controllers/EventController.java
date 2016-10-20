cc73d2a418381098e99a230c325d44317c4bfa8b
lars.maechel <lars.maechel@gmail.com>
2016-10-20 08:56:31 +0200

    Added EventController
    which fetches events from persistent store

--- src/main/java/com/event/presentation/controllers/EventController.java
+++ src/main/java/com/event/presentation/controllers/EventController.java
@@ -1,9 +1,40 @@
 package com.event.presentation.controllers;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.enterprise.context.ConversationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.event.business.session.services.RepositoryService;
+import com.event.domain.entities.Event;
+
+@Named
+@ConversationScoped
 public class EventController implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Inject
+	RepositoryService repositoryService;
 	
+	private Event selected;
+	private List<Event> events;
+	
+	public Event getSelected() {
+		return selected;
+	}
+	public void setSelected(Event selected) {
+		this.selected = selected;
+	}
+	
+	public List<Event> getEvents() {
+		if(events == null || events.isEmpty()) {
+			events = repositoryService.getEvents();
+		}
+		if(events == null || events.isEmpty()) {
+			System.out.println("***** events is null or empty ******");
+		}
+		return events;
+	}
 }
