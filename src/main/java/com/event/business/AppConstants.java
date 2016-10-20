07dbfdc8c16f43b246170cef216187ca971d2b1c
lars.maechel <lars.maechel@gmail.com>
2016-10-20 13:11:07 +0200

    Added Calendar credentials to AppConstants

--- src/main/java/com/event/business/AppConstants.java
+++ src/main/java/com/event/business/AppConstants.java
@@ -6,4 +6,9 @@ public class AppConstants {
 	private AppConstants() {}
 	
 	public final static String events_path = "https://dl.dropboxusercontent.com/u/35363454/javaee/lo1/material/events.txt";
+	
+	public final static String clientId = "1056802803092-rqvfnov3upnv0ljp7vgo5b0gu5kqnokh.apps.googleusercontent.com";
+	public final static String clientSecret = "ZVkoxA6fV4fqQ0a9XV9ig6Tq";
+	public final static String refreshToken= "1/Xx0SXrxKeIjt_5pyXihlFboY2bSUjjcTMmGizar53ioCD_Q7to7ShlEp0m4ClkS0";
+	
 }
