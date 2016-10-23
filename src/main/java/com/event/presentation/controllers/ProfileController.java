e238b48f591bef5ffe047e767d069301c77b940e
lars.maechel <lars.maechel@gmail.com>
2016-10-23 16:21:44 +0200

    implemented backing bean for profiles

--- src/main/java/com/event/presentation/controllers/ProfileController.java
+++ src/main/java/com/event/presentation/controllers/ProfileController.java
@@ -1,13 +1,34 @@
 package com.event.presentation.controllers;
 
 import java.io.Serializable;
+import java.util.List;
 
-import javax.enterprise.context.SessionScoped;
+import javax.enterprise.context.ConversationScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
+import com.event.business.session.services.RepositoryService;
+import com.event.domain.entities.User;
+
 @Named
-@SessionScoped
+@ConversationScoped
 public class ProfileController implements Serializable  {
 	private static final long serialVersionUID = 1L;
+	
+	@Inject 
+	RepositoryService repositoryService;
+	
+	private List<User> users;
 
+	public List<User> getUsers() {
+		if(users == null) {
+			users = repositoryService.getUsers();
+		}
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+	
 }
