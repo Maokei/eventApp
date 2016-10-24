133c49f6c0de8d3269d8d19ad44c4a8e787147e1
lars.maechel <lars.maechel@gmail.com>
2016-10-24 22:35:13 +0200

    Removed code relative to pageCounter in FileService

--- src/main/java/com/event/business/session/services/FileService.java
+++ src/main/java/com/event/business/session/services/FileService.java
@@ -1,7 +1,6 @@
 package com.event.business.session.services;
 
 import java.util.ArrayList;
-import java.util.concurrent.ConcurrentHashMap;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Singleton;
@@ -13,8 +12,6 @@ import javax.servlet.ServletContextEvent;
 import com.event.business.AppConstants;
 import com.event.business.resourcehandlers.FileHandler;
 import com.event.business.util.EventParser;
-import com.event.domain.entities.HitCounter;
-import com.event.presentation.counters.AtomicCounter;
 
 @Singleton
 @Startup
@@ -35,21 +32,5 @@ public class FileService {
 		final ServletContext servletContext = e.getServletContext();
 		final String contextRealPath = servletContext.getRealPath("/images/");
 		repository.persistEventsFromFile(parser, contextRealPath);
-
-		final String[] pages = { "error.jsp", "newEvent.jsp", "overview.jsp", "profile.jsp" };
-		for (final String page : pages) {
-			final HitCounter counter = new HitCounter();
-			counter.setPageReferenced(page);
-			//repository.persistHitCounter(counter);
-		}
-
-		
-		// final List<HitCounter> counters = service.findAllCounters();
-
-		final ConcurrentHashMap<String, AtomicCounter> counterMap = new ConcurrentHashMap<>();
-		/* for (final HitCounter counter : counters) {
-			counterMap.put(counter.getPageReferenced(), new AtomicCounter(counter.getCounter()));
-		} */
-		servletContext.setAttribute("counterMap", counterMap);
 	}
 }
