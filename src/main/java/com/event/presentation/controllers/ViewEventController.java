7b3ff8d0c2d62ee6af6d920bec523791127dc91f
lars.maechel <lars.maechel@gmail.com>
2016-10-24 20:52:27 +0200

    ViewEventController now sorts comments based on time

--- src/main/java/com/event/presentation/controllers/ViewEventController.java
+++ src/main/java/com/event/presentation/controllers/ViewEventController.java
@@ -1,12 +1,15 @@
 package com.event.presentation.controllers;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.enterprise.context.ConversationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import com.event.business.session.services.RepositoryService;
+import com.event.domain.entities.Comment;
 import com.event.domain.entities.Event;
 
 @Named
@@ -19,6 +22,7 @@ public class ViewEventController implements Serializable {
 	
 	private Integer id;
 	private Event event;
+	private List<Comment> comments;
 
 	public Integer getId() {
 		return id;
@@ -32,11 +36,24 @@ public class ViewEventController implements Serializable {
 		if(event == null) {
 			event = repositoryService.findEventById(id);
 		}
+		
 		return event;
 	}
 
 	public void setEvent(Event event) {
 		this.event = event;
 	}
+
+	public List<Comment> getComments() {
+		event.getComments().stream().sorted(
+				(e1, e2) -> e1.getDateTime().compareTo(e2.getDateTime()))
+				.forEach(comments::add);
+		return comments;
+	}
+
+	public void setComments(List<Comment> comments) {
+		this.comments = comments;
+	}
+	
 
 }
